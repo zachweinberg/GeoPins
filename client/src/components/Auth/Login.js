63b2c96de28b7f06c3c0a518516b10e19bc13720
Zach James <zjwgibsg@gmail.com>
2019-03-16 16:20:34 -0400

    apollo server, global react context for state and google auth

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,10 +1,45 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
+import React, { useContext } from "react"
+import { GraphQLClient } from "graphql-request"
+import { GoogleLogin } from "react-google-login"
+import { withStyles } from "@material-ui/core/styles"
 // import Typography from "@material-ui/core/Typography";
 
+import Context from "../../context"
+
+const ME_QUERY = `
+  {
+    me{
+      _id
+      name
+      email
+      picture
+    }
+  }
+`
+
 const Login = ({ classes }) => {
-  return <div>Login</div>;
-};
+  const { dispatch } = useContext(Context)
+
+  const onSuccess = async googleUser => {
+    const idToken = googleUser.getAuthResponse().id_token
+
+    const client = new GraphQLClient("http://localhost:4000/graphql", {
+      headers: { Authorization: idToken }
+    })
+
+    const data = await client.request(ME_QUERY)
+
+    dispatch({ type: "LOGIN_USER", payload: data.me })
+  }
+
+  return (
+    <GoogleLogin
+      clientId="769590735707-jrjnao2ojn17jg7sqvg7tb68dd85t0p8.apps.googleusercontent.com"
+      onSuccess={onSuccess}
+      isSignedIn={true}
+    />
+  )
+}
 
 const styles = {
   root: {
@@ -14,6 +49,6 @@ const styles = {
     flexDirection: "column",
     alignItems: "center"
   }
-};
+}
 
-export default withStyles(styles)(Login);
+export default withStyles(styles)(Login)
---
+++ server.js
@@ -0,0 +1,37 @@
+const { ApolloServer } = require("apollo-server")
+const mongoose = require("mongoose")
+require("dotenv").config()
+
+const typeDefs = require("./typeDefs")
+const resolvers = require("./resolvers")
+const { findOrCreateUser } = require("./controllers/userController")
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true
+  })
+  .then(() => console.log("DB connected"))
+  .catch(err => console.log(err))
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: async ({ req }) => {
+    let authToken = null
+    let currentUser = null
+
+    try {
+      authToken = req.headers.authorization
+
+      if (authToken) {
+        currentUser = await findOrCreateUser(authToken)
+      }
+    } catch (err) {
+      console.log(`Unable to authenticate user with token ${authToken}`)
+    }
+
+    return { currentUser }
+  }
+})
+
+server.listen().then(({ url }) => console.log(`Server live on port ${url}`))
